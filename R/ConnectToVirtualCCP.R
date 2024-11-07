95efe1eccfbdc1d1f0f8f7a7965b5ff0d826dbdd
BastianReiter <150595257+BastianReiter@users.noreply.github.com>
2024-11-07 02:30:59 +0100

    Ver 0.1.42: Temporarily back to old data model

--- R/ConnectToVirtualCCP.R
+++ R/ConnectToVirtualCCP.R
@@ -39,7 +39,8 @@ ConnectToVirtualCCP <- function(CCPTestData,
     LoginBuilder <- DSI::newDSLoginBuilder(.silent = FALSE)
 
     # Calculate auxiliary variables
-    AllPatientIDs <- CCPTestData$Patient$PatientID
+    AllPatientIDs <- CCPTestData$patient$"_id"
+    # AllPatientIDs <- CCPTestData$Patient$PatientID
     CountTotalPatients <- n_distinct(AllPatientIDs)
     PatientsPerSite <- floor(CountTotalPatients / NumberOfSites)
 
@@ -65,36 +66,52 @@ ConnectToVirtualCCP <- function(CCPTestData,
                                  size = PatientsPerSite)
 
         # Get data subsets that relate to sampled PatientIDs
-        SiteTestData <- list(BioSampling = NULL,
-                             Diagnosis = as.data.frame(filter(CCPTestData$Diagnosis, CCPTestData$Diagnosis$PatientID %in% SitePatientIDs)),
+        SiteTestData <- list(sample = as.data.frame(filter(CCPTestData$sample, CCPTestData$sample$"patient-id" %in% SitePatientIDs)),
+                             diagnosis = as.data.frame(filter(CCPTestData$diagnosis, CCPTestData$diagnosis$"patient-id" %in% SitePatientIDs)),
                              GeneralPerformance = NULL,
-                             Histology = as.data.frame(filter(CCPTestData$Histology, CCPTestData$Histology$PatientID %in% SitePatientIDs)),
-                             Metastasis = as.data.frame(filter(CCPTestData$Metastasis, CCPTestData$Metastasis$PatientID %in% SitePatientIDs)),
-                             MolecularDiagnostics = NULL,
+                             histology = as.data.frame(filter(CCPTestData$histology, CCPTestData$histology$"patient-id" %in% SitePatientIDs)),
+                             metastasis = as.data.frame(filter(CCPTestData$metastasis, CCPTestData$metastasis$"patient-id" %in% SitePatientIDs)),
+                             "molecular-marker" = NULL,
                              OtherClassification = NULL,
-                             Patient = as.data.frame(filter(CCPTestData$Patient, CCPTestData$Patient$PatientID %in% SitePatientIDs)),
-                             Progress = as.data.frame(filter(CCPTestData$Progress, CCPTestData$Progress$PatientID %in% SitePatientIDs)),
-                             RadiationTherapy = as.data.frame(filter(CCPTestData$RadiationTherapy, CCPTestData$RadiationTherapy$PatientID %in% SitePatientIDs)),
-                             Staging = as.data.frame(filter(CCPTestData$Staging, CCPTestData$Staging$PatientID %in% SitePatientIDs)),
-                             Surgery = as.data.frame(filter(CCPTestData$Surgery, CCPTestData$Surgery$PatientID %in% SitePatientIDs)),
-                             SystemicTherapy = as.data.frame(filter(CCPTestData$SystemicTherapy, CCPTestData$SystemicTherapy$PatientID %in% SitePatientIDs)),
+                             patient = as.data.frame(filter(CCPTestData$patient, CCPTestData$patient$"_id" %in% SitePatientIDs)),
+                             progress = as.data.frame(filter(CCPTestData$progress, CCPTestData$progress$"patient-id" %in% SitePatientIDs)),
+                             "radiation-therapy" = as.data.frame(filter(CCPTestData$"radiation-therapy", CCPTestData$"radiation-therapy"$"patient-id" %in% SitePatientIDs)),
+                             tnm = as.data.frame(filter(CCPTestData$tnm, CCPTestData$tnm$"patient-id" %in% SitePatientIDs)),
+                             surgery = as.data.frame(filter(CCPTestData$surgery, CCPTestData$surgery$"patient-id" %in% SitePatientIDs)),
+                             "system-therapy" = as.data.frame(filter(CCPTestData$"system-therapy", CCPTestData$"system-therapy"$"patient-id" %in% SitePatientIDs)),
                              TherapyRecommendation = NULL)
 
-        # *** TEMPORARY fix *** until table names are clear
-        names(SiteTestData) <- c("sample",
-                                 "diagnosis",
-                                 "GeneralPerformance",
-                                 "histology",
-                                 "metastasis",
-                                 "molecular-marker",
-                                 "OtherClassification",
-                                 "patient",
-                                 "progress",
-                                 "radiation-therapy",
-                                 "tnm",
-                                 "surgery",
-                                 "system-therapy",
-                                 "TherapyRecommendation")
+        # # Get data subsets that relate to sampled PatientIDs
+        # SiteTestData <- list(BioSampling = NULL,
+        #                      Diagnosis = as.data.frame(filter(CCPTestData$Diagnosis, CCPTestData$Diagnosis$PatientID %in% SitePatientIDs)),
+        #                      GeneralPerformance = NULL,
+        #                      Histology = as.data.frame(filter(CCPTestData$Histology, CCPTestData$Histology$PatientID %in% SitePatientIDs)),
+        #                      Metastasis = as.data.frame(filter(CCPTestData$Metastasis, CCPTestData$Metastasis$PatientID %in% SitePatientIDs)),
+        #                      MolecularDiagnostics = NULL,
+        #                      OtherClassification = NULL,
+        #                      Patient = as.data.frame(filter(CCPTestData$Patient, CCPTestData$Patient$PatientID %in% SitePatientIDs)),
+        #                      Progress = as.data.frame(filter(CCPTestData$Progress, CCPTestData$Progress$PatientID %in% SitePatientIDs)),
+        #                      RadiationTherapy = as.data.frame(filter(CCPTestData$RadiationTherapy, CCPTestData$RadiationTherapy$PatientID %in% SitePatientIDs)),
+        #                      Staging = as.data.frame(filter(CCPTestData$Staging, CCPTestData$Staging$PatientID %in% SitePatientIDs)),
+        #                      Surgery = as.data.frame(filter(CCPTestData$Surgery, CCPTestData$Surgery$PatientID %in% SitePatientIDs)),
+        #                      SystemicTherapy = as.data.frame(filter(CCPTestData$SystemicTherapy, CCPTestData$SystemicTherapy$PatientID %in% SitePatientIDs)),
+        #                      TherapyRecommendation = NULL)
+
+        # # *** TEMPORARY fix *** until table names are clear
+        # names(SiteTestData) <- c("sample",
+        #                          "diagnosis",
+        #                          "GeneralPerformance",
+        #                          "histology",
+        #                          "metastasis",
+        #                          "molecular-marker",
+        #                          "OtherClassification",
+        #                          "patient",
+        #                          "progress",
+        #                          "radiation-therapy",
+        #                          "tnm",
+        #                          "surgery",
+        #                          "system-therapy",
+        #                          "TherapyRecommendation")
 
 
         # 2) Build virtual server in global environment
